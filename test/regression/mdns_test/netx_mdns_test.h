bcca2fc99a650620072c879bfdb09762b914a9bd
Tinic Uro <tinicuro@gmail.com>
2026-08-19 12:22:20 -0700

    mdns: distinguish exhausted host conflicts

--- test/regression/mdns_test/netx_mdns_test.h
+++ test/regression/mdns_test/netx_mdns_test.h
@@ -82,6 +82,7 @@
 #define MDNS_LLA_DELETE                     0x6D    /* This entry deletes link local address. */
 #define MDNS_RECREATE                       0x6F    /* This entry deletes and creates mdns instance. */
 #define MDNS_WAIT_TICK                      0x70    /* This entry sleep specified ticks. */
+#define MDNS_SET_HOST_CONFLICT_LIMIT        0x71    /* This entry exhausts host conflict retries. */
 
 
 #define MDNS_FLAG_QUERY                     0x0000  /* Define flag for standard query. */
